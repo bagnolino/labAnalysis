5831d88d60ba500e77a8a85b9fde861f925d7c60
Elena Farè <elenafare@MacBook-Air-di-Elena.local>
2025-04-06 14:48:30 +0200

    Save Progress

--- codeProf/RLC_smorzC_v0.py
+++ codeProf/RLC_smorzC_v0.py
@@ -162,7 +162,6 @@ plt.savefig(file+'_1'+'.png',
             orientation ='Portrait',
             dpi = 100)
 
-plt.show()
 
 
 # Perform the first fit to define t0
@@ -244,9 +243,9 @@ Procediamo quindi allo shift rigido della curva e alla sua regressione sono in A
 shift= 0 # 2e-6
 Vout = Vout[tempo>t0_BF+shift]-v0_BF+0.0035 #Vout
 eVout = eVout[tempo>t0_BF+shift]
+print(eVout[:20])
 tempo = tempo[tempo>t0_BF+shift]-t0_BF
 N = len(tempo)
-print(tempo[:10])
 
 
 # Nuova regressione utilizzando fitf2 quindi sono nei parametri A,B e C, sempre con i minimi quadrati
@@ -283,7 +282,6 @@ plt.savefig(file+'_2'+'.png',
             orientation ='Portrait',
             dpi = 100)
 
-plt.show()
 
 
 # Extract and print best fit (BF) parameters and errors
